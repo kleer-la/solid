ca8da8f537f907be1a233d5b42ebe78d43a702be
Carlos Peix <carlos.peix@kleer.la>
2012-07-17 20:05:13 -0300

    Faltaba el modificador public de una clase

--- dot-net/src/LSP.MailBuilder/LSP.MailBuilder/Tests/MailBuilderTest.cs
+++ dot-net/src/LSP.MailBuilder/LSP.MailBuilder/Tests/MailBuilderTest.cs
@@ -6,7 +6,7 @@ using NUnit.Framework;
 namespace LSP.MailBuilder.Tests
 {
     [TestFixture]
-    class MailBuilderTest
+    public class MailBuilderTest
     {
         [Test]
         public void TestContactInformation()
